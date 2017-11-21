cf6f7efd2dbc42a07ba3eb5ca1c96c6c12c77bb8
Evensson <charlie.evensson@hotmail.com>
2017-11-21 16:16:11 +0100

    1

---
+++ CognitaSpel/Assets/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    public GameObject[] startMenu;
+
+
+    public void switchToNewMenu1()
+    {
+        foreach (GameObject ui in startMenu)
+        {
+            ui.SetActive(false);
+        }
+
+
+
+    }
+		
+	
+}
